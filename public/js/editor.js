1a64d2eabb7b057d44b5d7b502cf4b21553a196e
HyeokjinKang <coupy1024@icloud.com>
2025-10-19 21:44:10 +0900

    fix: gotoMain 함수 호출시 preventUnload가 true인 경우 메인으로 이동하지 못하는 문제

--- public/js/editor.js
+++ public/js/editor.js
@@ -723,7 +723,7 @@ const initialize = () => {
 };
 
 const gotoMain = (isCalledByMain) => {
-  if (isCalledByMain || (preventUnload && confirm("Are you sure you want to leave? There are unsaved changes."))) {
+  if (isCalledByMain || (preventUnload && confirm("Are you sure you want to leave? There are unsaved changes.")) || !preventUnload) {
     song.stop();
     song = new Howl({
       src: ["/sounds/tick.mp3"],
